b69212001a7745befa883f1532ced622695bf723
Masoud Gheysari M <khashmeshab@users.sourceforge.net>
2012-12-12 20:30:22 +0330

    - Added a configuration variable to enable or disable procssing of the PHP code execution (?{code}) tags. - Added a new HTTP header line for dynamic include files MIME types. - Removed the extra slash at the start of Megan URLs. - Template label values are now accessible using $Megan->Label method. - Fixed the problem of not replacing a tag if it's placed at the start of the template file. - ?{code} tags are now show the output of the PHP code using output buffering instead of the return value. - Changed the samples to show how to use ?{code} tags for executing a PHP code directly or inside a dynamic include.
    Signed-off-by: Masoud Gheysari M <khashmeshab@users.sourceforge.net>

--- Source/Megan/Megan.php
+++ Source/Megan/Megan.php
@@ -15,15 +15,18 @@
 	 * @name		Megan Template Engine
 	 * @author		Masoud Gheysari M <m.gheysari@gmail.com>
 	 * @copyright 	2011 - Masoud Gheysari M
-	 * @version 	1.2.4
+	 * @version 	1.2.5
 	 * @license		BSD
 	 */
 	
-	// Enable or disable dynamic includes
-	define('MEGAN_ENABLE_DYNAMIC'	,true);
-	 
+	
+	// Configuration
+	define('MEGAN_ENABLE_DYNAMIC'	,true); // Enable dynamic includes (~{file} tags)
+	define('MEGAN_ENABLE_CODE'		,true); // Enable PHP code executions (?{code} tags)
+ 
+	
+	// Process direct calls to this script for dynamic includes:
 	if(MEGAN_ENABLE_DYNAMIC) {
-		// Process direct calls to this script for dynamic includes:
 		// 1. Temporary turn off error reporting
 		$display_errors=ini_get('display_errors');
 		ini_set('display_errors','Off');
@@ -32,7 +35,8 @@
 
 		// 2. If this script is called directly, send the processed included file to the browser
 		if($_SERVER['SCRIPT_FILENAME']==__FILE__ && isset($_GET['MeganID'])) {
-			header('Content-Disposition: inline; filename='.$_SESSION['Megan'][$_GET['MeganID']]['Name']);
+			header('Content-Type: '.get_mime($_SESSION['Megan'][$_GET['MeganID']]['Path']));
+			header('Content-Disposition: inline; filename='.basename($_SESSION['Megan'][$_GET['MeganID']]['Path']));
 			header('Content-Length: '.strlen($_SESSION['Megan'][$_GET['MeganID']]['Data']));
 			echo $_SESSION['Megan'][$_GET['MeganID']]['Data'];
 			unset($_SESSION['Megan'][$_GET['MeganID']]);
@@ -42,7 +46,24 @@
 		// 3. Reset the error reporting status
 		ini_set('display_errors',$display_errors);
 	}
+	
+	function get_mime($file) {
+		$mime=false;
+		if(function_exists('finfo_file')) {
+			$finfo=finfo_open(FILEINFO_MIME_TYPE);
+			$mime=finfo_file($finfo, $file);
+			finfo_close($finfo);
+		} elseif(function_exists('mime_content_type')) {
+			$mime=mime_content_type($file);
+		} elseif(!stristr(ini_get('disable_functions'),'shell_exec')) {
+			$file=escapeshellarg($file);
+			$mime=shell_exec('file -bi '.$file);
+		}
+		return $mime;
+	}
 
+	
+	// Main class
 	class Megan {
 		private $megan_url,
 			$labels_array,
@@ -56,13 +77,17 @@
 			$www_root_dir=str_replace($_SERVER['SCRIPT_NAME'],'',$_SERVER['SCRIPT_FILENAME']);
 			$megan_script=str_replace($www_root_dir,'',__FILE__);
 			if(isset($_SERVER['HTTPS']))
-				$this->megan_url='https://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/'.$megan_script;
+				$this->megan_url='https://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].$megan_script;
 			else
-				$this->megan_url='http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/'.$megan_script;
+				$this->megan_url='http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].$megan_script;
 		}
 		
 		function __set($name,$value) {
 			$this->labels_array[$name]=$value;
+		}
+		
+		function __get($name) {
+			return $this->labels_array[$name];
 		}
 		
 		function __toString() {
@@ -80,7 +105,7 @@
 			// Embed additional files: @{file} tags
 			while(true) {
 				$i=strpos($template,'@{');
-				if(!$i) break;
+				if($i===false) break;
 				$j=strpos($template,'}',$i+2);
 				$tag=substr($template,$i+2,$j-$i-2);
 				$template=substr($template,0,$i).file_get_contents(dirname($this->template_file).'/'.$tag).substr($template,$j+1);
@@ -88,7 +113,7 @@
 			// Replace global labels: #{label} tags
 			while(true) {
 				$i=strpos($template,'#{');
-				if(!$i) break;
+				if($i===false) break;
 				$j=strpos($template,'}',$i+2);
 				$tag=substr($template,$i+2,$j-$i-2);
 				if(isset($this->labels_array[$tag]))
@@ -98,7 +123,7 @@
 			while(true) {
 				$sections="";
 				$i=strpos($template,'{{');
-				if(!$i) break;
+				if($i===false) break;
 				$j=strpos($template,'{',$i+2);
 				$tag=substr($template,$i+2,$j-$i-2);
 				$k=strpos($template,'}}'.$tag.'}',$j+2);
@@ -113,13 +138,13 @@
 				// Process dynamic includes: ~{file} tags
 				while(true) {
 					$i=strpos($template,'~{');
-					if(!$i) break;
+					if($i===false) break;
 					if(!isset($serialize)) $serialize=serialize($this);
 					$j=strpos($template,'}',$i+2);
 					$tag=substr($template,$i+2,$j-$i-2);
 					$token=md5($serialize.$tag);
 					$path=dirname($this->template_file).'/'.$tag;
-					$_SESSION['Megan'][$token]['Name']=basename($path);
+					$_SESSION['Megan'][$token]['Path']=$path;
 					$_SESSION['Megan'][$token]['Data']=$this->Generate(true,file_get_contents($path));
 					$template=substr($template,0,$i).$this->megan_url.'?MeganID='.$token.substr($template,$j+1);
 				}
@@ -127,17 +152,22 @@
 			// Replace local labels: ${label} tags
 			while(true) {
 				$i=strpos($template,'${');
-				if(!$i) break;
+				if($i===false) break;
 				$j=strpos($template,'}',$i+2);
 				$tag=substr($template,$i+2,$j-$i-2);
 				$template=substr($template,0,$i).$this->labels_array[$tag].substr($template,$j+1);
 			}
-			while(true) {
-				$i=strpos($template,'?{');
-				if(!$i) break;
-				$j=strpos($template,'}',$i+2);
-				$tag=substr($template,$i+2,$j-$i-2);
-				$template=substr($template,0,$i).eval($tag).substr($template,$j+1);
+			if(MEGAN_ENABLE_CODE) {
+				while(true) {
+					$i=strpos($template,'?{');
+					if($i===false) break;
+					$j=strpos($template,'}',$i+2);
+					$tag=substr($template,$i+2,$j-$i-2);
+					ob_start();
+					eval($tag);
+					$out=ob_get_clean();
+					$template=substr($template,0,$i).$out.substr($template,$j+1);
+				}
 			}
 			if($return)
 				return $template;
